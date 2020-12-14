2584a0cad9eadff5cd5ab578d9b139f193de41a7
Sergio Merces <sergio.merces@gmail.com>
2020-12-14 13:10:37 -0300

    Primeiro commit

---
+++ js/chat.js
@@ -0,0 +1,5 @@
+/*Script do chat do sistema de Auto Atendimento
+escrito por Sérgio da Costa Mercês*/
+
+let intro = window.document.querySelector('div.intro');
+intro.innerHTML='<p>Bem-vindo(a) ao serviço de Web Auto Atendimento do SAAE Salto!</p><p>Qual o serviço desejado?</p>';
